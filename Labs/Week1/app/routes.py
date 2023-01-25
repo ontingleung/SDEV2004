89049d9efcd1aa36787bca2dd0ccf9b225bbb5de
ontingleung <ontingleung@tutamail.com>
2023-01-25 12:06:38 +0000

    week1 lab

--- Labs/Week1/app/routes.py
+++ Labs/Week1/app/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World from Lab Class Week 1!"
+    user = {'username': 'Onting Leung'}
+    return render_template('index.html', title='Lab Class Week 1', user=user)
