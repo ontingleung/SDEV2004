f1eeeb6beb8f350bb9754ceb807eb609fa7f805d
ontingleung <ontingleung@tutamail.com>
2023-02-08 23:47:12 +0000

    week2a

--- Labs/Week2/app/routes.py
+++ Labs/Week2/app/routes.py
@@ -5,4 +5,20 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Onting'}
-    return render_template('index.html', title='Lab Class Week 1', user=user)
+    # return "Welcome to Lab Class WEEK TWO -Let the fun begin!"
+    labclasses = [
+        {
+            'weekno': 'one',
+            'content': 'Getting started with Flask!'
+        },
+        {
+            'weekno': 'two',
+            'content': 'Doing more with templates!'
+        }
+    ]
+    return render_template('index.html', title='LabTWO', user=user, labclasses=labclasses)
+
+# @app.route('/data_input/', methods=('GET', 'POST'))
+# def data_input():
+#     user = {'username': request.form.get('username')}
+#     return render_template('index.html', title='Week 2', user=user)
